109890f509c2da3e852fb9d2e1ad65e4a1f9bce6
Darrion White <84244339+whid72@users.noreply.github.com>
2022-06-13 13:39:12 -0400

    init

---
+++ starter/MovieBrowser/Source/Detail/MovieDetailViewController.swift
@@ -0,0 +1,11 @@
+//
+//  MovieDetailViewController.swift
+//  SampleApp
+//
+//  Created by Struzinski, Mark on 2/26/21.
+//  Copyright © 2021 Lowe's Home Improvement. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailViewController: UIViewController { }
---
+++ starter/MovieBrowser/Source/Search/SearchViewController.swift
@@ -0,0 +1,11 @@
+//
+//  SearchViewController.swift
+//  SampleApp
+//
+//  Created by Struzinski, Mark on 2/19/21.
+//  Copyright © 2021 Lowe's Home Improvement. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {}
